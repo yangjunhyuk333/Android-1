f806cae546654cec80ed9d4d34dbaf7961b8c9e8
yang0925 <keun0602@naver.com>
2020-09-23 18:08:13 +0900

    [ADD] searchAdapter

---
+++ HiSchool/app/src/main/java/com/example/hischool/data/SearchRecyclerViewData.kt
@@ -0,0 +1,14 @@
+package com.example.hischool.data
+
+class SearchRecyclerViewData (
+    val profile : Int,
+    val nickname : String,
+    val time : String,
+    val more : Int,
+    val title : String,
+    val question : String,
+    val heart : Int,
+    val count_heart : Int,
+    val message : String,
+    val count_message : Int
+)
