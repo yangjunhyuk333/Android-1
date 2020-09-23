e94a7b25b04910f860f5cc846cfca49adf50bf92
yang0925 <keun0602@naver.com>
2020-09-23 19:43:06 +0900

    [UPDATE] search

--- HiSchool/app/src/main/java/com/example/hischool/adapter/SearchAdapter.kt
+++ HiSchool/app/src/main/java/com/example/hischool/adapter/SearchAdapter.kt
@@ -8,7 +8,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.hischool.R
 import com.example.hischool.data.SearchRecyclerViewData
-import kotlinx.android.synthetic.main.search_item.view.*
 
 class SearchAdapter (val searchList : ArrayList<SearchRecyclerViewData>) : RecyclerView.Adapter<SearchAdapter.ViewHolder>() {
 
@@ -22,19 +21,24 @@ class SearchAdapter (val searchList : ArrayList<SearchRecyclerViewData>) : Recyc
     }
 
     override fun onBindViewHolder(holder: SearchAdapter.ViewHolder, position: Int) {
-        holder.profile
+        holder.profile.setImageResource(searchList.get(position).profile)
+        holder.nickname.text = searchList.get(position).nickname
+        holder.time.text = searchList.get(position).time
+        holder.title.text = searchList.get(position).title
+        holder.question.text = searchList.get(position).question
+        holder.count_heart.text = searchList.get(position).count_heart.toString()
+        holder.count_message.text = searchList.get(position).count_message.toString()
     }
 
     class ViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView){
         val profile = itemView.findViewById<ImageView>(R.id.search_profile_image)
         val nickname = itemView.findViewById<TextView>(R.id.search_name_text)
         val time = itemView.findViewById<TextView>(R.id.search_time_text)
-        val more = itemView.findViewById<ImageView>(R.id.search_more_btn)
         val title = itemView.findViewById<TextView>(R.id.search_title_text)
         val question = itemView.findViewById<TextView>(R.id.search_question_text)
-        val heart = itemView.findViewById<ImageView>(R.id.search_heart_btn)
         val count_heart = itemView.findViewById<TextView>(R.id.search_count_heart_text)
-        val message = itemView.findViewById<ImageView>(R.id.search_message_btn)
         val count_message = itemView.findViewById<TextView>(R.id.search_count_message_text)
     }
 }
+
+
--- HiSchool/app/src/main/java/com/example/hischool/data/SearchRecyclerViewData.kt
+++ HiSchool/app/src/main/java/com/example/hischool/data/SearchRecyclerViewData.kt
@@ -4,11 +4,8 @@ class SearchRecyclerViewData (
     val profile : Int,
     val nickname : String,
     val time : String,
-    val more : Int,
     val title : String,
     val question : String,
-    val heart : Int,
     val count_heart : Int,
-    val message : String,
     val count_message : Int
 )
